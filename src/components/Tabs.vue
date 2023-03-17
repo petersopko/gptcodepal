e3ae12641596eab4220d7360bcd48afeaf290215
Peter Sopko <petersopko@protonmail.com>
2023-03-17 19:34:06 +0100

    semi working tabs

---
+++ src/components/Tabs.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="tabs">
+        <div v-for="(tab, index) in tabs" :key="index" class="tab" :class="{ active: activeTab === index }">
+            <span class="tab-name" @click.stop="setActiveTab(index)">{{ index }}</span>
+            <span class="delete-tab" @click.stop="deleteTab(index)">&#10005;</span>
+        </div>
+        <div class="tab add-tab" @click="addTab">
+            <span class="plus-sign">+</span>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: {
+        tabs: Array,
+        activeTab: Number,
+    },
+    methods: {
+        setActiveTab(index) {
+            this.$emit("update:activeTab", index);
+        },
+        addTab() {
+            this.$emit("add-tab");
+        },
+        deleteTab(index) {
+            this.$emit("delete-tab", index);
+        },
+    },
+};
+</script>
+
+<style scoped>
+.tabs {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+}
+
+.tab,
+.add-tab {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 15px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    cursor: pointer;
+    min-width: 80px;
+}
+
+.tab.active,
+.add-tab:hover {
+    background-color: #ccc;
+}
+
+.tab-name {
+    margin-right: 10px;
+}
+
+.delete-tab {
+    font-size: 20px;
+    font-weight: bold;
+    cursor: pointer;
+    border: 1px solid #ccc;
+    border-radius: 50%;
+    padding: 2px 5px;
+}
+
+.plus-sign {
+    font-size: 20px;
+    font-weight: bold;
+    margin: 0 auto;
+}
+</style>
--- src/views/Interface.vue
+++ src/views/Interface.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container">
     <Loader :loading="loading" />
+    <Tabs :tabs="tabs" :activeTab="activeTabIndex" @update:activeTab="activeTabIndex = $event" @add-tab="addTab"
+      @delete-tab="deleteTab" />
     <TextInput v-model="description" placeholder="Enter your description" class="input" :rows="5" />
     <CodeInputList :codeInputs="codeInputs" @remove="removeCodeInput" @add="addCodeInput" />
     <div class="flex justify-between items-center">
@@ -12,9 +14,8 @@
   </div>
 </template>
 
-
 <script setup>
-import { ref } from "vue";
+import { ref, reactive, watch } from "vue";
 import Loader from "../components/Loader.vue";
 import Settings from "../components/Settings.vue";
 import TextInput from "../components/TextInput.vue";
@@ -22,6 +23,7 @@ import CodeInputList from "../components/CodeInputList.vue";
 import SubmitButton from "../components/SubmitButton.vue";
 import TokenEstimations from "../components/TokenEstimations.vue";
 import ResponseSection from "../components/ResponseSection.vue";
+import Tabs from "../components/Tabs.vue";
 import useSubmitPrompt from "../composables/useSubmitPrompt.js";
 import useDescription from "../composables/useDescription.js";
 import useCodeInputs from "../composables/useCodeInputs.js";
@@ -37,6 +39,45 @@ const saveApiKey = (key) => {
   localStorage.setItem("openai_api_key", key);
   apiKey.value = key;
 };
-
+function deleteTab(index) {
+  tabs.splice(index, 1);
+  activeTabIndex.value = Math.min(activeTabIndex.value, tabs.length - 1);
+  localStorage.setItem("tabs", JSON.stringify(tabs));
+}
 const { submitPrompt, response, loading, actualTokens, responseTokens } = useSubmitPrompt(apiKey, description, codeInputs);
+
+const tabs = reactive(
+  JSON.parse(localStorage.getItem("tabs") || '[{ "description": "", "response": "" }]')
+);
+const activeTabIndex = ref(0);
+watch(
+  () => tabs[activeTabIndex.value],
+  (tab) => {
+    description.value = tab.description;
+    response.value = tab.response;
+  },
+  { immediate: true }
+);
+
+function addTab() {
+  tabs.push({ description: "", response: "" });
+  activeTabIndex.value = tabs.length - 1;
+  localStorage.setItem("tabs", JSON.stringify(tabs));
+}
+
+watch(
+  () => description.value,
+  (newDescription) => {
+    tabs[activeTabIndex.value].description = newDescription;
+    localStorage.setItem("tabs", JSON.stringify(tabs));
+  }
+);
+
+watch(
+  () => response.value,
+  (newResponse) => {
+    tabs[activeTabIndex.value].response = newResponse;
+    localStorage.setItem("tabs", JSON.stringify(tabs));
+  }
+);
 </script>
