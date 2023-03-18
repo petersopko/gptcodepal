f907512cf191eea1c50a205ab571e36631a80961
Peter Sopko <petersopko@protonmail.com>
2023-03-18 11:12:36 +0100

    naive apikeyinput

---
+++ src/components/ApiKeyInput.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="flex items-center">
+        <label for="api-key-input" class="font-bold mr-2">OpenAI API Key:</label>
+        <div class="relative w-full max-w-md">
+            <n-input id="api-key-input" v-model:value="apiKey" type="password" show-password-on="mousedown"
+                placeholder="API Key" />
+        </div>
+        <n-button class="mx-2" @click="saveApiKey">
+            Save API Key
+        </n-button>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { NInput } from "naive-ui";
+import { NButton } from "naive-ui";
+
+const emit = defineEmits(["save-api-key"]);
+
+const apiKey = ref("");
+
+onMounted(() => {
+    const savedApiKey = localStorage.getItem("openai_api_key");
+    if (savedApiKey) {
+        apiKey.value = savedApiKey;
+    }
+});
+
+const saveApiKey = () => {
+    localStorage.setItem("openai_api_key", apiKey.value);
+    emit("save-api-key", apiKey.value);
+};
+</script>
