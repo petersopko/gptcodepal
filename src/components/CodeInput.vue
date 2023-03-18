99b6c625c5567b022318793b4e4edb550b84b03b
Peter Sopko <petersopko@protonmail.com>
2023-03-18 10:47:56 +0100

    naive code input icon buttons

--- src/components/CodeInput.vue
+++ src/components/CodeInput.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="bg-gray-800 border border-gray-300 p-4 rounded-none mb-4">
-        <div class="flex items-center justify-between text-xl font-bold mb-">
+        <div class="flex items-center justify-between text-xl font-bold mb-4">
             <div>
                 <span>{{ !modelValue.name ? 'Code Input ' + (index + 1) : modelValue.name }}</span>
             </div>
             <div>
-                <button @click="toggleFold" class="text-lg text-white p-2 rounded-none mx-4">
-                    <font-awesome-icon :icon="['fas', `chevron-${isFolded ? 'down' : 'up'}`]" />
-                </button>
-                <button @click="$emit('remove')" class="text-lg text-red p-2 rounded-none">
+                <n-button @click="toggleFold" class="text-lg text-white p-2 rounded-none mx-4">
+                    <font-awesome-icon :icon="['fas', `chevron-${refIsFolded ? 'down' : 'up'}`]" />
+                </n-button>
+                <n-button @click="$emit('remove')" class="text-lg text-red p-2 rounded-none">
                     <font-awesome-icon icon="trash-alt" />
-                </button>
+                </n-button>
             </div>
         </div>
         <div v-show="!refIsFolded">
