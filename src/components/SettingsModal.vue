63cef2c2d2849b3da212f9d0a35729da6dbf6318
Peter Sopko <petersopko@protonmail.com>
2023-03-24 22:57:53 +0100

    centering settings modal

--- src/components/SettingsModal.vue
+++ src/components/SettingsModal.vue
@@ -7,39 +7,32 @@
     class="custom-card"
     preset="card"
     :style="bodyStyle"
-    title="Modal"
     :bordered="false"
     size="huge"
     :segmented="segmented"
   >
-    <template #header-extra> Oops! </template>
-    <n-grid>
-      <n-gi class="flex justify-center">
-        <n-statistic label="Prompts🪙" :value="promptTokensTotal" />
-      </n-gi>
-      <n-gi class="flex justify-center">
-        <n-statistic label="Completions🪙" :value="completionTokensTotal" />
-      </n-gi>
-      <n-gi class="flex justify-center items-center">
-        <n-statistic label="# of Prompts🤖" :value="totalPromptsSent" />
-      </n-gi>
-      <n-gi class="flex justify-center items-center">
-        <n-statistic label="Money 💲" :value="totalMoneySpent" />
-      </n-gi>
-    </n-grid>
-    <div class="flex justify-center">
-      <n-gradient-text>Max tokens for answer (tooltip here)</n-gradient-text>
+    <!-- <template #header-extra> Oops! </template> -->
+    <div class="flex justify-center items-center space-x-10">
+      <n-statistic label="Prompts🪙" :value="promptTokensTotal" />
+      <n-statistic label="Completions🪙" :value="completionTokensTotal" />
+      <n-statistic label="# of Prompts🤖" :value="totalPromptsSent" />
+      <n-statistic label="Money 💲" :value="totalMoneySpent" />
     </div>
-    <div class="flex justify-center">
-      <n-input-number
-        placeholder="Max tokens for answer"
-        v-model:value="maxTokens"
-        clearable
-        min="0"
-        @update:value="updateMaxTokens"
-      />
-    </div>
-    <template #footer> Footer </template>
+
+    <template #footer>
+      <div class="flex justify-center">
+        <n-gradient-text>Max tokens for answer (tooltip here)</n-gradient-text>
+      </div>
+      <div class="flex justify-center">
+        <n-input-number
+          placeholder="Max tokens for answer"
+          v-model:value="maxTokens"
+          clearable
+          min="0"
+          @update:value="updateMaxTokens"
+        />
+      </div>
+    </template>
   </n-modal>
 </template>
 
