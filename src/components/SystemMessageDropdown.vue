e61db9c960140a42eb860e589ab8ea6b30ca91d8
Peter Sopko <petersopko@protonmail.com>
2023-04-01 22:00:39 +0200

    systemMessageDropdown 2

--- src/components/SystemMessageDropdown.vue
+++ src/components/SystemMessageDropdown.vue
@@ -1,13 +1,26 @@
 <template>
-  <n-dropdown trigger="hover" placement="bottom-start" :options="options" @select="handleSelect">
-    <n-button>{{ sysMsgStore.selectedSystemMessage.label }}</n-button>
-  </n-dropdown>
+  <div class="flex justify-center">
+    <n-dropdown trigger="hover" placement="bottom-start" :options="options" @select="handleSelect">
+      <n-button> {{ sysMsgStore.selectedSystemMessage.label }} </n-button>
+    </n-dropdown>
+    <n-tooltip placement="bottom-start" trigger="hover">
+      <template #trigger>
+        <n-button class="ml-4">
+          <n-icon>
+            <CogOutline />
+          </n-icon>
+        </n-button>
+      </template>
+      Customization coming soon!
+    </n-tooltip>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
-import { NDropdown, NButton } from 'naive-ui'
+import { NDropdown, NButton, NIcon, NTooltip } from 'naive-ui'
 import { useSystemMessages } from '@/stores/systemMessages'
+import { CogOutline } from '@vicons/ionicons5'
 
 const sysMsgStore = useSystemMessages()
 
